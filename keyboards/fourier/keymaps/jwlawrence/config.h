60b9ad678e26234590d70c4994dffa751e728f06
Joshua Lawrence <jlawrence@linkedin.com>
2018-09-07 14:21:46 -0700

    Updates to jwlawrence keymaps
    * Changed keymapping for zen
    * Changed keymapping for redox
    * Changed keymapping for fourier

--- keyboards/fourier/keymaps/jwlawrence/config.h
+++ keyboards/fourier/keymaps/jwlawrence/config.h
@@ -1,5 +1,8 @@
 /*
-Copyright 2017 Danny Nguyen <danny@hexwire.com>
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -24,18 +27,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL
 // #define USE_I2C
-
-/* Select hand configuration */
-
-// #define MASTER_LEFT
-#define MASTER_RIGHT
-// #define EE_HANDS
-
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 16
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
 
 #endif
