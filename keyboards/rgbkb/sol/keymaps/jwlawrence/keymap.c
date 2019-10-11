0a8f6bcbce1b4f101753ebe4ebaa52c38f7c82f2
Joshua Lawrence <jlawrence@linkedin.com>
2019-10-11 14:05:51 -0700

    Updated keymaps for jwlawrence
    - Updated keymaps for zen and sol

--- keyboards/rgbkb/sol/keymaps/jwlawrence/keymap.c
+++ keyboards/rgbkb/sol/keymaps/jwlawrence/keymap.c
@@ -37,22 +37,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|------|  |------|------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |   [  |  |   ]  |   Y  |   U  |   I  |   O  |   P  |   \  |
    * |------+------+------+------+------+------|------|  |------|------+------+------+------+------+------|
-   * |FN(CAPS)| A  |   S  |   D  |   F  |   G  |   (  |  |   )  |   H  |   J  |   K  |   L  |   ;  |   '  |
+   * | `    | A    |   S  |   D  |   F  |   G  |   (  |  |   )  |   H  |   J  |   K  |   L  |   ;  |   '  |
    * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-   * |Shift |   Z  |   X  |   C  |   V  |   B  |   {  |  |   }  |   N  |   M  |   ,  |   .  |   /  |Enter |
+   * |Shift |   Z  |   X  |   C  |   V  |   B  |   {  |  |   }  |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-   * | Ctrl |  Win |  Alt |  RGB | ADJ  | Space| DEL  | | Enter| Space|  FN  | Left | Down | Up   |Right |
+   * |  FN  | ADJ  |  RGB | Ctrl | Alt  |  GUI | Alt  |  | Enter| Space|  FN  | Left | Down |  Up  |Right |
    * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------'
-   *                                    | Space| DEL  |  | Enter| Space|
+   *                                    |  GUI | Alt  |  | Enter| Space|
    *                                    `-------------'  `-------------'
    */
   [_QWERTY] = LAYOUT( \
-    KC_GESC,    KC_1,    KC_2,    KC_3,    KC_4,     KC_5, KC_MINS,  KC_EQL,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC, \
-    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,      KC_T, KC_LBRC,  KC_RBRC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS, \
-    KC_GRV,    KC_A,    KC_S,    KC_D,    KC_F,      KC_G, KC_LPRN,  KC_RPRN,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, \
-    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,     KC_B, KC_LCBR,  KC_RCBR,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, RGB_TOG,     ADJ,  KC_SPC,  KC_LCTL,  KC_ENT,  KC_SPC,     KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT,  FN,  \
-                                                  KC_SPC,  KC_LCTL,  KC_ENT,  KC_SPC \
+    KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_MINS,     KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC,     KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+    KC_GRV,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_LPRN,     KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LCBR,     KC_RCBR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    FN,       ADJ,     RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, KC_LALT,  KC_ENT,  KC_SPC  ,  FN,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  \
+                                                  KC_LGUI, KC_LALT,  KC_ENT,  KC_SPC \
   ),
 
   /* Colemak
@@ -72,10 +72,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_COLEMAK] = LAYOUT( \
     KC_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_MINS,  KC_EQL,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC, \
-     KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G, KC_LBRC, KC_RBRC,    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSLS, \
-    FN_CAPS,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D, KC_LPRN, KC_RPRN,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT, \
-    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LCBR, KC_RCBR,    KC_K,    KC_M, KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT, \
-    KC_LCTL, KC_LGUI, KC_LALT, RGB_TOG,     ADJ,  KC_SPC,  KC_DEL,  KC_ENT,  KC_SPC,      FN, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, \
+     KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G, KC_LBRC,  KC_RBRC,   KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSLS, \
+    FN_CAPS,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D, KC_LPRN,  KC_RPRN,   KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT, \
+    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LCBR,  KC_RCBR,   KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT, \
+    KC_LCTL, KC_LGUI, KC_LALT, RGB_TOG,    ADJ,   KC_SPC,  KC_DEL,  KC_ENT,  KC_SPC,      FN, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, \
                                                   KC_SPC,  KC_DEL,  KC_ENT,  KC_SPC \
   ),
 
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |  |      |      |      |RGBTOG|  HUI |  SAI | VAI  |
    * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-   * |      |      |      |RGBMOD(|      |      |      |  |      |      |     |RGBRMOD| HUD |  SAD | VAD  |
+   * |      |      |      |RGBMOD|      |      |      |  |      |      |      |RGBRMOD| HUD |  SAD | VAD  |
    * `------+------+------+------+------+------+------|  |------+------+------+------+------+------+------'
    *                                    |      |      |  |      |      |
    *                                    `-------------'  `-------------'
